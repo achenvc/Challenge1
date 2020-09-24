4b76d27069e5b84314e4e5eccde5b50ee8f355f2
Angela Chen <arcchessqueen@gmail.com>
2020-09-24 14:42:10 -0400

    challenge 1

--- Challenge1/ContentView.swift
+++ Challenge1/ContentView.swift
@@ -9,8 +9,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let temperaturePicker = ["Celcius", "Fahrenheit", "Kelvin"]
+    let temps = [UnitTemperature.celsius, UnitTemperature.fahrenheit, UnitTemperature.kelvin]
+    @State private var input = ""
+    @State private var temperaturePickNumInput = 0
+    @State private var temperaturePickNumOutput = 0
+    
+    var resultValue: Double {
+        let value = Double(input) ?? 0
+        let inputVal = Measurement(value: value, unit: temps[temperaturePickNumInput])
+        
+        return inputVal.converted(to: temps[temperaturePickNumOutput]).value
+    }
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            Form {
+                Section(header: Text("What unit are you starting with?")) {
+                   Picker("Temperature", selection: $temperaturePickNumInput) {
+                        ForEach(0 ..< temperaturePicker.count) {
+                            Text("\(self.temperaturePicker[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                    TextField("Put number here", text: $input).keyboardType(.decimalPad)
+                    
+                    
+                }
+                Section(header: Text("What unit do you want to end with?")) {
+                   Picker("Temperature", selection: $temperaturePickNumOutput) {
+                        ForEach(0 ..< temperaturePicker.count) {
+                            Text("\(self.temperaturePicker[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                }
+                Section(header: Text("Resulting value")) {
+                    Text("\(resultValue)")
+                }
+            }
+        }.navigationBarTitle("WeSplit")
     }
 }
 
